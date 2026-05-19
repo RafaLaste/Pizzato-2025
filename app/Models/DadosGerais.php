8ffd56116be2866dc9f790c6337eee5a40368d5c
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-19 16:56:10 -0300

    feat: adiciona DadosGerais model

---
+++ app/Models/DadosGerais.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DadosGerais extends Model
+{
+    protected $table = 'dados_gerais';
+
+    public $timestamps = false;
+}
